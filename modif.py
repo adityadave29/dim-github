9fa4807597673ee371480d9b50e6ce85a80ec173
Aditya Dave <cool2912004@gmail.com>
2023-05-10 23:23:09 +0530

    w

--- modif.py
+++ modif.py
@@ -13,11 +13,11 @@ while True:
 
     k = cv2.waitKey(1)
 
-    if k % 256 == 27:
-        # For Esc key
-        print("Close")
-        break
-    elif k % 256 == 32:
+    # if k % 256 == 27:
+    #     # For Esc key
+    #     print("Close")
+    #     break
+    if k % 256 == 32:
         # For Space key
         print("Image saved")
         file = '/home/pi/dim-github/img.jpg'
