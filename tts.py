f82306d59c2ecd2b175fc9cb482dce27784270c9
Aditya Dave <cool2912004@gmail.com>
2023-05-10 18:11:05 +0530

    hello

--- tts.py
+++ tts.py
@@ -1,14 +1,10 @@
 import pyttsx3
-import cv2
 
-def run_text_speech(text, count):
+def text_to_speech(text):
     text_speech = pyttsx3.init()
     text_speech.say(text)
     text_speech.runAndWait()
-    cv2.waitKey(5)
-    print("Asking for new image no: ", count)
 
-# Example usage
-text = "Hello, Raspberry Pi!"
-count = 1
-run_text_speech(text, count)
+if __name__ == "__main__":
+    text = "Hello, this is a test."
+    text_to_speech(text)
