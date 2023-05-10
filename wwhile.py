9806d09b12c2fb2e5505d19f24c536ea26984432
Aditya Dave <cool2912004@gmail.com>
2023-05-10 22:11:21 +0530

    w

--- wwhile.py
+++ wwhile.py
@@ -1,29 +1,84 @@
+# import cv2
+# import pytesseract
+# import pyttsx3
+
+# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
+# def capture_image(count):
+#     cam = cv2.VideoCapture(0)
+#     print("Hello")
+#     ret, img = cam.read()
+#     cv2.imshow("Test", img)
+
+#     if not ret:
+#         return
+
+#     k = cv2.waitKey(0)
+
+#     if k % 256 == 27:
+#         # For Esc key
+#         print("Close")
+#     elif k % 256 == 32:
+#         # For Space key
+#         print("Image " + str(count) + "saved")
+#         file = '/home/pi/dim-github/img' + str(count) + '.jpg'
+#         cv2.imwrite(file, img)
+#         count += 1
+
+#     cam.release()
+#     cv2.destroyAllWindows()
+
+#     img = cv2.imread('img' + str(count - 1) + '.jpg')
+#     cv2.imshow('sample img', img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+#     text = pytesseract.image_to_string(img)
+#     print(text)
+
+#     def speak(text):
+#         engine = pyttsx3.init()
+#         engine.setProperty('rate', 150)  # Speed of speech
+#         engine.say(text)
+#         engine.runAndWait()
+
+#     speak(text)
+#     print("Asking for new image no: ", count)
+#     return count
+
+# count = 0
+# while True:
+#     count = capture_image(count)
+
+
 import cv2
 import pytesseract
 import pyttsx3
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-
-def capture_image(count):
-    cam = cv2.VideoCapture(0)
+count = 0
+cam = cv2.VideoCapture(0)
+while True:
     print("Hello")
-    ret, img = cam.read()
-    cv2.imshow("Test", img)
+    while True:
+        ret, img = cam.read()
+        cv2.imshow("Test", img)
 
-    if not ret:
-        return
+        if not ret:
+            break
 
-    k = cv2.waitKey(0)
+        k = cv2.waitKey(1)
 
-    if k % 256 == 27:
-        # For Esc key
-        print("Close")
-    elif k % 256 == 32:
-        # For Space key
-        print("Image " + str(count) + "saved")
-        file = '/home/pi/dim-github/img' + str(count) + '.jpg'
-        cv2.imwrite(file, img)
-        count += 1
+        if k % 256 == 27:
+            # For Esc key
+            print("Close")
+            break
+        elif k % 256 == 32:
+            # For Space key
+            print("Image " + str(count) + "saved")
+            file = '/home/pi/dim-github/img' + str(count) + '.jpg'
+            cv2.imwrite(file, img)
+            count += 1
 
     cam.release()
     cv2.destroyAllWindows()
@@ -36,16 +91,20 @@ def capture_image(count):
     text = pytesseract.image_to_string(img)
     print(text)
 
+    # text_speech = pyttsx3.init()
+    # text_speech.say(text)
+    # text_speech.runAndWait()
+    # cv2.waitKey(5)
+        
+    # import os
+    # os.system('espeak "{}"'.format(text))
+
     def speak(text):
         engine = pyttsx3.init()
         engine.setProperty('rate', 150)  # Speed of speech
         engine.say(text)
         engine.runAndWait()
-
     speak(text)
-    print("Asking for new image no: ", count)
-    return count
 
-count = 0
-while True:
-    count = capture_image(count)
+    
+    print("Asking for new image no: ", count)
