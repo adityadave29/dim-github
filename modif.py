12db380cf1874a9010ed0998e90fc615e2875228
Aditya Dave <cool2912004@gmail.com>
2023-05-10 22:42:50 +0530

    w

--- modif.py
+++ modif.py
@@ -2,10 +2,9 @@ import cv2
 import pytesseract
 import pyttsx3
 
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd=r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 count = 0
 cam = cv2.VideoCapture(0)
-
 while True:
     print("Hello")
     while True:
@@ -15,37 +14,33 @@ while True:
         if not ret:
             break
 
-        k = cv2.waitKey(1)
+        k=cv2.waitKey(1)
 
-        if k % 256 == 27:
-            # For Esc key
+        if k%256==27:
+            #For Esc key
             print("Close")
             break
-        elif k % 256 == 32:
-            # For Space key
-            print("Image " + str(count) + " saved")
-            file = '/home/pi/dim-github/img' + str(count) + '.jpg'
-            cv2.imwrite(file, img)
-            count += 1
+        elif k%256==32:
+            #For Space key
 
-    # Move these lines outside of the inner while loop
-    cam.release()
+            print("Image "+str(count)+"saved")
+            file='C:/Users/91931/Desktop/DIM/img'+str(count)+'.jpg'
+            cv2.imwrite(file, img)
+            count +=1
+
+    cam.release
+    cv2.destroyAllWindows
+
+    img = cv2.imread('img'+str(count -1)+'.jpg')
+    cv2.imshow('sample img',img)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    img = cv2.imread('img' + str(count - 1) + '.jpg')
-    if img is not None and img.size[0] > 0 and img.size[1] > 0:
-        cv2.imshow('sample img', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
     text = pytesseract.image_to_string(img)
-    print(text)
+    print(text) 
 
-    def speak(text):
-        engine = pyttsx3.init()
-        engine.setProperty('rate', 150)  # Speed of speech
-        engine.say(text)
-        engine.runAndWait()
-    speak(text)
-
+    text_speech = pyttsx3.init()
+    text_speech.say(text)
+    text_speech.runAndWait()
+    cv2.waitKey(5)
     print("Asking for new image no: ", count)
