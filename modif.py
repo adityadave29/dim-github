e322d7178c02087b14da80649757d02638a551a8
Aditya Dave <cool2912004@gmail.com>
2023-05-10 23:36:15 +0530

    w

--- modif.py
+++ modif.py
@@ -4,40 +4,43 @@ import pyttsx3
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 cam = cv2.VideoCapture(0)
+space_pressed = False
+
 while True:
     ret, img = cam.read()
     cv2.imshow("Test", img)
-    
+
     if not ret:
         break
 
     k = cv2.waitKey(1)
 
-    # if k % 256 == 27:
-    #     # For Esc key
-    #     print("Close")
-    break 
-    if k == 256:
+    if k % 256 == 27:
+        # For Esc key
+        print("Close")
+        break
+    elif k % 256 == 32:
         # For Space key
-        
         print("Image saved")
         file = '/home/pi/dim-github/img.jpg'
         cv2.imwrite(file, img)
+        space_pressed = True
 
-cam.release()
-cv2.destroyAllWindows()
+    if space_pressed:
+        cv2.destroyAllWindows()
+        img = cv2.imread('img.jpg')
+        cv2.imshow('sample img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-img = cv2.imread('img.jpg')
-cv2.imshow('sample img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        text = pytesseract.image_to_string(img)
+        print(text)
 
-text = pytesseract.image_to_string(img)
-print(text)
+        def speak(text):
+            engine = pyttsx3.init()
+            engine.setProperty('rate', 150)  # Speed of speech
+            engine.say(text)
+            engine.runAndWait()
 
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # Speed of speech
-    engine.say(text)
-    engine.runAndWait()
-speak(text)
+        speak(text)
+        break
