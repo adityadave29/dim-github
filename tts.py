7fb6ee51e41f4e587079f67634a7f2838a4ca798
Aditya Dave <cool2912004@gmail.com>
2023-05-10 18:15:40 +0530

    hello

--- tts.py
+++ tts.py
@@ -1,10 +1,6 @@
 import pyttsx3
 
-def text_to_speech(text):
-    text_speech = pyttsx3.init()
-    text_speech.say(text)
-    text_speech.runAndWait()
-
-if __name__ == "__main__":
-    text = "Hello, this is a test."
-    text_to_speech(text)
+text = "Hello, Raspberry Pi!"
+text_speech = pyttsx3.init()
+text_speech.say(text)
+text_speech.runAndWait()
