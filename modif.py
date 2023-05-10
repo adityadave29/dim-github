952d3cf49cbfd34af4c057ba1afc6bc11124344c
Aditya Dave <cool2912004@gmail.com>
2023-05-10 22:59:56 +0530

    w

--- modif.py
+++ modif.py
@@ -11,13 +11,7 @@ while True:
     if not ret:
         break
 
-    k = cv2.waitKey(1)
-
-    if k % 256 == 27:
-        # For Esc key
-        print("Close")
-        break
-    elif k % 256 == 32:
+    else:
         # For Space key
         print("Image saved")
         file = '/home/pi/dim-github/img.jpg'
@@ -27,20 +21,12 @@ cam.release()
 cv2.destroyAllWindows()
 
 img = cv2.imread('img.jpg')
-cv2.imshow('sample img', img)
-cv2.waitKey(0)
+# cv2.imshow('sample img', img)
+# cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 text = pytesseract.image_to_string(img)
 print(text)
-
-# text_speech = pyttsx3.init()
-# text_speech.say(text)
-# text_speech.runAndWait()
-# cv2.waitKey(5)
-    
-# import os
-# os.system('espeak "{}"'.format(text))
 
 def speak(text):
     engine = pyttsx3.init()
