79dbc4835e32447a0b6ee67f85374545d43430e6
Aditya Dave <cool2912004@gmail.com>
2023-05-10 18:17:59 +0530

    hello

--- tts.py
+++ tts.py
@@ -1,6 +1,9 @@
-import pyttsx3
-
-text = "Hello, Raspberry Pi!"
-text_speech = pyttsx3.init()
-text_speech.say(text)
-text_speech.runAndWait()
+import pyttsx
+engine= pyttsx.init()
+engine.setProperty('rate',70)
+voices=engine.getProperty('voices')
+for voice in voices:
+    print ("Using voice:"), repr(voice)
+    engine.setProperty('voice',voice.id)
+    engine.say("Hello Hello Hello")
+engine.runAndWait()
