cdaa88e7d6d25f0d387a0eeee5662b7e080f3eda
Aditya Dave <cool2912004@gmail.com>
2023-05-10 19:57:59 +0530

    hello

--- tts.py
+++ tts.py
@@ -8,5 +8,18 @@
 #     engine.say("Hello Hello Hello")
 # engine.runAndWait()
 
-import os
-os.system('espeak "{}"'.format(text))
+# import os
+# 
+# os.system('espeak "{}"'.format(text))
+
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Speed of speech
+    engine.say(text)
+    engine.runAndWait()
+
+# Example usage
+text = "Hello, Raspberry Pi. Text to speech is working!"
+speak(text)
