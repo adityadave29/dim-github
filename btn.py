76f2efb47bd054857da91d64c818b4a5e4fd73bb
Aditya Dave <cool2912004@gmail.com>
2023-05-11 12:05:24 +0530

    w

--- btn.py
+++ btn.py
@@ -1,29 +1,42 @@
 from gpiozero import Button
 import os
-file = open("count.txt","w")
+
+# Initialize count to 0
+file = open("count.txt", "w")
 file.write(str(0))
 file.close()
 
 def call_script():
-    count = open("count.txt","r")
+    count = open("count.txt", "r")
     cnt = int(count.read())
     count.close()
-    if(cnt % 2 == 0):
+    
+    if cnt % 2 == 0:
         print("Button pressed. Calling script...")
-        os.system("python3 modif.py")
+        os.system("python3 modify.py")
     else:
-        os.system("Stoping....")
+        print("Stopping....")
+        # Create an empty file as a signal to stop the modify.py script
+        stop_file = open("stop.txt", "w")
+        stop_file.close()
+    
     cnt += 1
-    count = open("count.txt","w")
+    count = open("count.txt", "w")
     count.write(str(cnt))
     count.close()
 
 button = Button(17)
-
 button.when_pressed = call_script
 
 while True:
-    pass
+    # Check if the stop.txt file exists, indicating a request to stop the modify.py script
+    if os.path.exists("stop.txt"):
+        os.remove("stop.txt")  # Remove the stop.txt file
+        print("Received stop signal. Exiting modify.py script.")
+        break  # Break out of the while True loop
+
+    # Rest of the main file's code
+
 
 
 # from gpiozero import Button
