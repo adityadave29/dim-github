298a1e5ce3dd7ba7a94374d8ab53dc4994039414
Aditya Dave <cool2912004@gmail.com>
2023-05-11 10:00:46 +0530

    w

--- modif.py
+++ modif.py
@@ -1,11 +1,15 @@
 import cv2
 import pytesseract
 import pyttsx3
+import time
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 cam = cv2.VideoCapture(0)
 
-while True:
+start_time = time.time()
+duration = 10  # Duration of the webcam feed in seconds
+
+while time.time() - start_time < duration:
     ret, img = cam.read()
     cv2.imshow("Test", img)
 
@@ -18,8 +22,7 @@ cv2.destroyAllWindows()
 img = cv2.imread('img.jpg')
 cv2.imshow('sample img', img)
 
-# Wait for a fixed time (e.g., 5 seconds) before closing the window
-cv2.waitKey(5000)
+time.sleep(5)  # Display the sample image for 5 seconds
 cv2.destroyAllWindows()
 
 text = pytesseract.image_to_string(img)
