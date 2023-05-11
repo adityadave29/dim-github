39b6d8b3711645c2e8cb85be613b3bc5041a221f
Aditya Dave <cool2912004@gmail.com>
2023-05-11 17:50:10 +0530

    w

--- btn1.py
+++ btn1.py
@@ -4,7 +4,7 @@ import os
 
 def call_script():
     print("Button pressed. Calling script...")
-    os.system("python3 modif.py")
+    os.system("python3 btn2.py")
 
 button = Button(17)
 button.when_pressed = call_script
