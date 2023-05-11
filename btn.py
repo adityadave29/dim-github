4c80e1a843691917fdf7afe977433148ea242b9d
Aditya Dave <cool2912004@gmail.com>
2023-05-11 10:17:21 +0530

    w

---
+++ btn.py
@@ -0,0 +1,12 @@
+from gpiozero import Button
+import os
+
+def call_script():
+    print("Button pressed. Calling script...")
+    os.system("python3 modif.py")
+
+button = Button(17)
+button.when_pressed = call_script
+
+while True:
+    pass
