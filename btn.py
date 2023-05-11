783960f7d27ec3bc689a305690d1dc0aed766ada
Aditya Dave <cool2912004@gmail.com>
2023-05-11 11:51:07 +0530

    w

--- btn.py
+++ btn.py
@@ -1,14 +1,34 @@
+# from gpiozero import Button
+
+# import os
+
+# def call_script():
+#     print("Button pressed. Calling script...")
+#     os.system("python3 modif.py")
+
+# button = Button(17)
+
+# button.when_pressed = call_script
+
+# while True:
+#     pass
 from gpiozero import Button
 import os
 
+button_press_count = 0
+
 def call_script():
+    global button_press_count
+    button_press_count += 1
     print("Button pressed. Calling script...")
-    os.system("python3 modif.py")
+    if button_press_count == 1:
+        os.system("python3 modif.py")
+    elif button_press_count == 2:
+        print("Button pressed again. Stopping the program.")
+        exit()
 
 button = Button(17)
 button.when_pressed = call_script
 
 while True:
-    if button.is_pressed:
-        print("Button pressed again. Stopping the program.")
-        break
+    pass
