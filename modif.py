548e9110c4d4f18d8f6ebef83c2660fcab2527dc
Aditya Dave <cool2912004@gmail.com>
2023-05-11 10:06:42 +0530

    w

--- modif.py
+++ modif.py
@@ -1,53 +1,9 @@
-# import cv2
-# import pytesseract
-# import pyttsx3
-
-# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-# cam = cv2.VideoCapture(0)
-# while True:
-#     ret, img = cam.read()
-#     cv2.imshow("Test", img)
-
-#     if not ret:
-#         break
-
-#     k = cv2.waitKey(1)
-
-#     if k % 256 == 27:
-#         # For Esc key
-#         print("Close")
-#         break
-#     elif k % 256 == 32:
-#         # For Space key
-#         print("Image saved")
-#         file = '/home/pi/dim-github/img.jpg'
-#         cv2.imwrite(file, img)
-
-# cam.release()
-# cv2.destroyAllWindows()
-
-# img = cv2.imread('img.jpg')
-# cv2.imshow('sample img', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# text = pytesseract.image_to_string(img)
-# print(text)
-
-# def speak(text):
-#     engine = pyttsx3.init()
-#     engine.setProperty('rate', 150)  # Speed of speech
-#     engine.say(text)
-#     engine.runAndWait()
-# speak(text)
-
 import cv2
 import pytesseract
 import pyttsx3
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 cam = cv2.VideoCapture(0)
-
 while True:
     ret, img = cam.read()
     cv2.imshow("Test", img)
@@ -55,16 +11,25 @@ while True:
     if not ret:
         break
 
-    file = '/home/pi/dim-github/img.jpg'
-    cv2.imwrite(file, img)
+    k = cv2.waitKey(1)
+
+    if k % 256 == 27:
+        # For Esc key
+        print("Close")
+        break
+    elif k % 256 == 32:
+        # For Space key
+        print("Image saved")
+        file = '/home/pi/dim-github/img.jpg'
+        cv2.imwrite(file, img)
 
 cam.release()
 cv2.destroyAllWindows()
 
 img = cv2.imread('img.jpg')
-cv2.imshow('sample img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow('sample img', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 text = pytesseract.image_to_string(img)
 print(text)
@@ -75,3 +40,5 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 speak(text)
+
+
