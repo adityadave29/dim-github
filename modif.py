b5efdfc743cde408be2f022677cca36e5ed472a2
Aditya Dave <cool2912004@gmail.com>
2023-05-11 11:19:53 +0530

    w

--- modif.py
+++ modif.py
@@ -1,32 +1,80 @@
+# import cv2
+# import pytesseract
+# import pyttsx3
+
+# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+# cam = cv2.VideoCapture(0)
+# while True:
+#     ret, img = cam.read()
+#     cv2.imshow("Test", img)
+
+#     if not ret:
+#         break
+
+#     k = cv2.waitKey(1)
+
+#     if k % 256 == 32:
+#         # For Space key
+#         print("Image saved")
+#         file = '/home/pi/dim-github/img.jpg'
+#         cv2.imwrite(file, img)
+#         print("Close")
+#         break
+
+# cam.release()
+# cv2.destroyAllWindows()
+
+# img = cv2.imread('img.jpg')
+# # cv2.imshow('sample img', img)
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
+
+# text = pytesseract.image_to_string(img)
+# print(text)
+
+# def speak(text):
+#     engine = pyttsx3.init()
+#     engine.setProperty('rate', 150)  # Speed of speech
+#     engine.say(text)
+#     engine.runAndWait()
+# speak(text)
+
 import cv2
 import pytesseract
 import pyttsx3
-from gpiozero import Button
+import RPi.GPIO as GPIO
 
-def capture_image():
-    cam = cv2.VideoCapture(0)
-    while True:
-        ret, img = cam.read()
-        cv2.imshow("Test", img)
+# Set up GPIO
+button_pin = 18  # GPIO pin number for the button
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        if not ret:
-            break
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+cam = cv2.VideoCapture(0)
 
-        else:
-            print("Image saved")
-            file = '/home/pi/dim-github/img.jpg'
-            cv2.imwrite(file, img)
-            print("Close")
-            break
+while True:
+    ret, img = cam.read()
+    cv2.imshow("Test", img)
 
-    cam.release()
-    cv2.destroyAllWindows()
+    if not ret:
+        break
 
-    img = cv2.imread('img.jpg')
-    text = pytesseract.image_to_string(img)
-    print(text)
+    button_state = GPIO.input(button_pin)
 
-    speak(text)
+    if button_state == GPIO.LOW:
+        # Button pressed
+        print("Image saved")
+        file = '/home/pi/dim-github/img.jpg'
+        cv2.imwrite(file, img)
+        print("Close")
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
+img = cv2.imread('img.jpg')
+text = pytesseract.image_to_string(img)
+print(text)
 
 def speak(text):
     engine = pyttsx3.init()
@@ -34,10 +82,4 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-# Set up the GPIO button and callback function
-button = Button(18)  # Replace 17 with the GPIO pin number you are using
-button.when_pressed = capture_image
-
-# Keep the script running
-while True:
-    pass
+speak(text)
