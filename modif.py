8683924ee86027995f6b0c27171be7707582c73f
Aditya Dave <cool2912004@gmail.com>
2023-05-11 12:17:58 +0530

    w

--- modif.py
+++ modif.py
@@ -39,57 +39,47 @@
 #     engine.runAndWait()
 # speak(text)
 
-from builtins import exit
 import cv2
 import pytesseract
 import pyttsx3
 import RPi.GPIO as GPIO
-from gpiozero import Button
 
 # Set up GPIO
 button_pin = 18  # GPIO pin number for the button
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def killOf():
-    exit()
-    return
-button = Button(17)
-button.when_pressed = killOf
-
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-print("modif")
+cam = cv2.VideoCapture(0)
+
 while True:
-    cam = cv2.VideoCapture(0)
+    ret, img = cam.read()
+    cv2.imshow("Test", img)
 
-    while True:
-        ret, img = cam.read()
-        cv2.imshow("Test", img)
+    if not ret:
+        break
 
-        if not ret:
-            break
+    button_state = GPIO.input(button_pin)
 
-        button_state = GPIO.input(button_pin)
+    if button_state == GPIO.LOW:
+        # Button pressed
+        print("Image saved")
+        file = '/home/pi/dim-github/img.jpg'
+        cv2.imwrite(file, img)
+        print("Close")
+        break
 
-        if button_state == GPIO.LOW:
-            # Button pressed
-            print("Image saved")
-            file = '/home/pi/dim-github/img.jpg'
-            cv2.imwrite(file, img)
-            print("Close")
-            break
+cam.release()
+cv2.destroyAllWindows()
 
-    cam.release()
-    cv2.destroyAllWindows()
+img = cv2.imread('img.jpg')
+text = pytesseract.image_to_string(img)
+print(text)
 
-    img = cv2.imread('img.jpg')
-    text = pytesseract.image_to_string(img)
-    print(text)
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Speed of speech
+    engine.say(text)
+    engine.runAndWait()
 
-    def speak(text):
-        engine = pyttsx3.init()
-        engine.setProperty('rate', 150)  # Speed of speech
-        engine.say(text)
-        engine.runAndWait()
-
-    speak(text)
+speak(text)
