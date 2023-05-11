2d3b9350fa8f41b14a2e203650bfad1448fd2f5f
Aditya Dave <cool2912004@gmail.com>
2023-05-11 10:04:59 +0530

    w

--- modif.py
+++ modif.py
@@ -1,28 +1,69 @@
+# import cv2
+# import pytesseract
+# import pyttsx3
+
+# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+# cam = cv2.VideoCapture(0)
+# while True:
+#     ret, img = cam.read()
+#     cv2.imshow("Test", img)
+
+#     if not ret:
+#         break
+
+#     k = cv2.waitKey(1)
+
+#     if k % 256 == 27:
+#         # For Esc key
+#         print("Close")
+#         break
+#     elif k % 256 == 32:
+#         # For Space key
+#         print("Image saved")
+#         file = '/home/pi/dim-github/img.jpg'
+#         cv2.imwrite(file, img)
+
+# cam.release()
+# cv2.destroyAllWindows()
+
+# img = cv2.imread('img.jpg')
+# cv2.imshow('sample img', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# text = pytesseract.image_to_string(img)
+# print(text)
+
+# def speak(text):
+#     engine = pyttsx3.init()
+#     engine.setProperty('rate', 150)  # Speed of speech
+#     engine.say(text)
+#     engine.runAndWait()
+# speak(text)
+
 import cv2
 import pytesseract
 import pyttsx3
-import time
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 cam = cv2.VideoCapture(0)
 
-start_time = time.time()
-duration = 10  # Duration of the webcam feed in seconds
-
-while time.time() - start_time < duration:
+while True:
     ret, img = cam.read()
     cv2.imshow("Test", img)
 
     if not ret:
         break
 
+    file = '/home/pi/dim-github/img.jpg'
+    cv2.imwrite(file, img)
+
 cam.release()
 cv2.destroyAllWindows()
 
 img = cv2.imread('img.jpg')
 cv2.imshow('sample img', img)
-
-time.sleep(5)  # Display the sample image for 5 seconds
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 text = pytesseract.image_to_string(img)
@@ -33,5 +74,4 @@ def speak(text):
     engine.setProperty('rate', 150)  # Speed of speech
     engine.say(text)
     engine.runAndWait()
-
 speak(text)
