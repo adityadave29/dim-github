20da98de969ec130af3b57b5570f9034ec750bab
Aditya Dave <cool2912004@gmail.com>
2023-05-13 11:30:18 +0530

    repo

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,118 +1,57 @@
-# import RPi.GPIO as GPIO
-# import time
-
-# GPIO.setmode(GPIO.BCM)
-
-# # Define GPIO pins
-# TRIG_PIN = 23
-# ECHO_PIN = 24
-# BUZZER_PIN = 18
-
-# # Set up GPIO pins
-# GPIO.setup(TRIG_PIN, GPIO.OUT)
-# GPIO.setup(ECHO_PIN, GPIO.IN)
-# GPIO.setup(BUZZER_PIN, GPIO.OUT)
-
-# def calculate_distance():
-#     # Send 10us pulse to trigger pin
-#     GPIO.output(TRIG_PIN, True)
-#     time.sleep(0.00001)
-#     GPIO.output(TRIG_PIN, False)
-
-#     # Record time of pulse
-#     while GPIO.input(ECHO_PIN) == False:
-#         start_time = time.time()
-
-#     while GPIO.input(ECHO_PIN) == True:
-#         end_time = time.time()
-
-#     # Calculate distance
-#     duration = end_time - start_time
-#     distance = duration * 17150 # Speed of sound in cm/s
-#     distance = round(distance, 2)
-
-#     return distance
-
-# def trigger_buzzer(distance, threshold):
-#     if distance < threshold:
-#         GPIO.output(BUZZER_PIN, True)
-#     else:
-#         GPIO.output(BUZZER_PIN, False)
-
-# try:
-#     while True:
-#         distance = calculate_distance()
-#         print(f"Distance: {distance} cm")
-#         trigger_buzzer(distance, 100) # Trigger buzzer if distance is less than 10 cm
-#         time.sleep(0.5)
-
-# finally:
-#     GPIO.cleanup()
-
 import RPi.GPIO as GPIO
 import time
 
-
 button_pin = 26  # GPIO pin number for the button
-exit_pin = 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(exit_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+running = False
 
-button_state = GPIO.input(button_pin)	
-exit_state = GPIO.input(exit_pin)
+def button_callback(channel):
+    global running
+    running = not running
 
-if button_state == GPIO.LOW:
-# set GPIO numbering mode to BCM
-	
-	GPIO.setmode(GPIO.BCM)
+GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
 
-	# set up pins for sensor and buzzer
-	trig = 18
-	echo = 24
-	buzzer = 16
+trig = 18
+echo = 24
+buzzer = 16
 
-	# set up GPIO direction for sensor and buzzer
-	GPIO.setup(trig, GPIO.OUT)
-	GPIO.setup(echo, GPIO.IN)
-	GPIO.setup(buzzer, GPIO.OUT)
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+GPIO.setup(buzzer, GPIO.OUT)
 
-	def get_distance():
-		# trigger sensor to send ultrasonic signal
-		GPIO.output(trig, True)
-		time.sleep(0.00001)
-		GPIO.output(trig, False)
+def get_distance():
+    GPIO.output(trig, True)
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
 
-		# measure time for ultrasonic signal to bounce back
-		pulse_start = time.time()
-		pulse_end = time.time()
-		while GPIO.input(echo) == 0:
-			pulse_start = time.time()
-		while GPIO.input(echo) == 1:
-			pulse_end = time.time()
+    pulse_start = time.time()
+    pulse_end = time.time()
+    while GPIO.input(echo) == 0:
+        pulse_start = time.time()
+    while GPIO.input(echo) == 1:
+        pulse_end = time.time()
 
-		# calculate distance in centimeters
-		pulse_duration = pulse_end - pulse_start
-		distance = pulse_duration * 17150
-		return distance
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    return distance
 
-	try:
-		while True:
-			distance = get_distance()
-			print("Distance: %.2f cm" % distance)
+try:
+    while True:
+        if running:
+            distance = get_distance()
+            print("Distance: %.2f cm" % distance)
 
-			# turn on buzzer if object is within 50 cm
-			if distance < 50:
-				GPIO.output(buzzer, True)
-			else:
-				GPIO.output(buzzer, False)
+            if distance < 50:
+                GPIO.output(buzzer, True)
+            else:
+                GPIO.output(buzzer, False)
 
-			time.sleep(0.5)
+            time.sleep(0.5)
+        else:
+            GPIO.output(buzzer, False)
+            time.sleep(0.1)
 
-	except KeyboardInterrupt:
-		GPIO.cleanup()
-
-if exit_state == GPIO.LOW:
-        print("Exiting...")
-        exit()
+except KeyboardInterrupt:
+    GPIO.cleanup()
