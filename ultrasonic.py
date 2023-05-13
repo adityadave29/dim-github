a7c7710c02f2ba621e5de69749049e7344d9628f
Aditya Dave <cool2912004@gmail.com>
2023-05-13 11:16:27 +0530

    repo

--- ultrasonic.py
+++ ultrasonic.py
@@ -56,7 +56,7 @@ import time
 # GPIO.setmode(GPIO.BCM)
 # GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-button_state = GPIO.input(button_pin)	
+# button_state = GPIO.input(button_pin)	
 # if button_state == GPIO.LOW:
 # set GPIO numbering mode to BCM
 GPIO.setmode(GPIO.BCM)
